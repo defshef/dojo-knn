392351dd25a205b423fa5d82e78c8a9533d6a428
Paul Brabban <paul.brabban@gmail.com>
2017-02-25 12:58:19 +0000

    basic working code

--- src/defshef_dojo_knn/core.clj
+++ src/defshef_dojo_knn/core.clj
@@ -6,10 +6,14 @@
    url
    slurp
    string/split-lines
-   (map #(string/split % #","))))
+   (map #(string/split % #"[,]"))
+   (map (fn [row] {:features (map #(Double/parseDouble %) (butlast row)) :label (last row)}))))
 
-(defn split-randomized-dataset [k dataset]
-  (partition-all k (shuffle dataset)))
+(defn randomize [dataset]
+  (shuffle dataset))
+
+(defn dataset->folds [k dataset]
+  (partition-all k dataset))
 
 (defn train-test-seq
   ([folds]
@@ -17,33 +21,66 @@
   ([remaining [holdout & training]]
    (when (< 0 remaining)
      (lazy-seq
-      (cons {:holdout holdout :training training} (train-test-seq (dec remaining) (conj training holdout)))))))
+      (cons {:holdout-set holdout :training-set (apply concat training)}
+            (train-test-seq (dec remaining) (conj training holdout)))))))
+
+(defn sqrt [x] (Math/sqrt x))
+
+(defn pow [x y] (Math/pow x y))
+
+(defn euclidian-distance-between [a b]
+  (assert (= (count a) (count b)) "a and b must be the same size!")
+  (->>
+   (map (comp #(pow % 2) -) a b)
+   (reduce + 0)
+   sqrt))
 
 (defn train-classifier
-  "Train the classifier on the dataset. Produces a parametrized classifier."
-  [training-dataset])
+  "Train the classifier for k neighbours on the dataset. Produces a
+  parametrized classifier function that takes a feature vector and
+  returns a predicted label."
+  [k training-dataset]
+  (fn [datum]
+    (->>
+     training-dataset
+     (map (fn [{:keys [features label]}]
+            {:distance (euclidian-distance-between datum features)
+             :label label}))
+     (sort-by :distance)
+     (take k)
+     (group-by :label)
+     (sort-by (fn [[_ neighbours-with-label]] (- (count neighbours-with-label))))
+     first
+     first)))
 
 (defn test-classifier
-  "Test a trained classifier against a labelled dataset. Produces a percentage correct predictions."
-  [model test-dataset]
-  0)
+  "Test a trained classifier against a labelled dataset. Produces a
+  ratio of correct predictions vs. total predictions."
+  [classify test-dataset]
+  (let [correct-predictions (->>
+                             test-dataset
+                             (map (fn [test-datum]
+                                    (= (:label test-datum) (classify (:features test-datum)))))
+                             (filter true?)
+                             count)]
+    (/ correct-predictions (count test-dataset))))
 
-(defn perform-step [{:keys [holdout training]}]
-  (->>
-   training
-   train-classifier
-   (test-classifier holdout)))
+(defn perform-step [k {:keys [holdout-set training-set] :as arg}]
+  (let [trained-classifier (train-classifier k training-set)]
+    (test-classifier trained-classifier holdout-set)))
 
 (defn amean [coll]
   (/ (reduce + coll) (count coll)))
 
-(defn run-experiment [folds]
+(defn evaluate-performance
+  "Perform a leave-one-out validation for kNN algorithm with k neighbours"
+  [k folds]
   (let [results (reduce
                  (fn [results step-dataset]
                    (if step-dataset
-                     (conj results (perform-step step-dataset))
+                     (conj results (perform-step k step-dataset))
                      results))
                  []
-                 (train-test-seq folds))]
+                 folds)]
     {:results results
-     :mean (amean results)}))
+     :mean (double (amean results))}))
